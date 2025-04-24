3676d07cf9cad2a2adc0d07b66f24b76b6dde187
Yazmin Chozo <u20221b313@upc.edu.pe>
2025-04-23 22:34:25 -0500

    Initial commit

---
+++ src/main/java/pe/edu/upc/demopillcontrol/DemoPillControlApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.demopillcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPillControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPillControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upc/demopillcontrol/DemoPillControlApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.demopillcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPillControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
