864a4624531e3648a3d44f119bc103d96eba4aa8
Yazmin Chozo <142949518+yazrxc@users.noreply.github.com>
2025-04-29 16:28:29 -0500

    Revert "Creacion tabla farmacia"

--- LuisErik/LuisErik/src/main/java/pe/edu/upc/luiserik/repositories/FarmaciaRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.luiserik.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.luiserik.entities.Farmacia;
-
-@Repository
-public interface FarmaciaRepository extends JpaRepository<Farmacia,Long> {
-}
